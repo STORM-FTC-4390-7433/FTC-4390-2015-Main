dc43ba59d1521622a6fd8c76657af99dad1c6054
risheetnair <risheetnair@outlook.com>
2015-12-15 17:20:42 -0500

    Updated Autonomous

--- ftc_app-master/FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/PushBotAuto.java
+++ ftc_app-master/FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/PushBotAuto.java
@@ -4,8 +4,9 @@ public class PushBotAuto extends PushBotTelemetry
 {
     DcMotor motorRight;
     DcMotor motorLeft;
-    public long start, elapsed;
-    final static int milliPerTile = 1000;
+    public long start, currentTime;
+
+    final static int milliPerTile = 750;
     public PushBotAuto ()
     {
         start();
@@ -18,41 +19,43 @@ public class PushBotAuto extends PushBotTelemetry
     {
         motorRight = hardwareMap.dcMotor.get("RIGHT");
         motorLeft = hardwareMap.dcMotor.get("LEFT");
+
         switch (v_state)
         {
             case 0:
                 v_state++;
+                start = System.currentTimeMillis();
                 break;
             case 1:
-                start = System.currentTimeMillis();
                 motorRight.setPower(-1.0);
                 motorLeft.setPower(1.0);
                 if (System.currentTimeMillis() - start >= 2 * milliPerTile)
                 {
                     v_state ++;
                 }
-                long currentTime = System.currentTimeMillis() - start;
-                telemetry.addData("19", "Current Time: " + currentTime);
+                currentTime = System.currentTimeMillis() - start;
+                telemetry.addData("19", "The Current Time is: " + currentTime);
                 break;
 
 
             case 2:
+                motorRight.setPower(1.0);
+                motorLeft.setPower(1.0);
                 if(System.currentTimeMillis() - start >= 3 * milliPerTile)
                 {
-                    motorRight.setPower(1.0);
-                    motorLeft.setPower(1.0);
+                    v_state++;
                 }
                 break;
 
             case 3:
+                motorRight.setPower(-1.0);
+                motorLeft.setPower(1.0);
                 if (System.currentTimeMillis() - start >= 5 * milliPerTile)
                 {
-                    motorRight.setPower(-1.0);
-                    motorLeft.setPower(1.0);
+                    motorRight.setPower(0);
+                    motorLeft.setPower(0);
+                    v_state ++;
                 }
-                motorRight.setPower(0);
-                motorLeft.setPower(0);
-                v_state ++;
                 break;
 
             default:
